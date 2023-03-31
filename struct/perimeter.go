d1049affcd7e1e2324823bb1f8df4cdbf0596531
Michel Kim <kim98.michel@gmail.com>
2023-03-30 23:08:01 -0300

    refactor ours table driven test

--- struct/perimeter.go
+++ struct/perimeter.go
@@ -15,12 +15,21 @@ type Circle struct {
 	Radius float64
 }
 
-type Form interface {
-	Area() float64
-}
-
 func (c Circle) Area() (result float64) {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Height float64
+	Base   float64
+}
+
+func (t Triangle) Area() (result float64) {
+	return (t.Base * t.Height) * 0.5
+}
+
+type Form interface {
+	Area() float64
 }
 
 func Perimeter(rectangle Rectangle) (result float64) {
--- struct/perimeter_test.go
+++ struct/perimeter_test.go
@@ -20,14 +20,15 @@ func TestArea(t *testing.T) {
 		form   Form
 		expect float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
+		{form: Rectangle{Width: 12, Height: 6}, expect: 72.0},
+		{form: Circle{Radius: 10}, expect: 314.1592653589793},
+		{form: Triangle{Height: 12, Base: 6}, expect: 36.0},
 	}
 
 	for _, tt := range testsArea {
 		result := tt.form.Area()
 		if result != tt.expect {
-			t.Errorf("resultado %.2f, esperado %.2f", result, tt.expect)
+			t.Errorf("%#v resultado %.2f, esperado %.2f", tt.form, result, tt.expect)
 		}
 	}
 
