7e56e6a92313aa1654caac8467affa379ac93e77
Michel Kim <kim98.michel@gmail.com>
2023-03-30 22:57:52 -0300

    introduce Table Driven Tests

--- struct/perimeter_test.go
+++ struct/perimeter_test.go
@@ -16,25 +16,19 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	verifyFloatError := func(t *testing.T, form Form, expect float64) {
-		t.Helper()
-		result := form.Area()
-		if result != expect {
-			t.Errorf("result '%.2f', expect '%.2f'", result, expect)
+	testsArea := []struct {
+		form   Form
+		expect float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range testsArea {
+		result := tt.form.Area()
+		if result != tt.expect {
+			t.Errorf("resultado %.2f, esperado %.2f", result, tt.expect)
 		}
 	}
 
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		expect := 72.0
-
-		verifyFloatError(t, rectangle, expect)
-	})
-
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10}
-		expect := 314.1592653589793
-
-		verifyFloatError(t, circle, expect)
-	})
 }
